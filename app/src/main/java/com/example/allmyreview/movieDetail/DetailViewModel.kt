09766754eee3ae1ce0f2d56ce561f3713056bfff
jSY49 <96411866+jSY49@users.noreply.github.com>
2023-02-15 16:00:02 +0900

    Tmdb Api_ 영화세부사항 retrofit

---
+++ app/src/main/java/com/example/allmyreview/movieDetail/DetailViewModel.kt
@@ -0,0 +1,36 @@
+package com.example.allmyreview.movieDetail
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.allmyreview.DetailMovie
+import com.example.allmyreview.MovieResult
+import com.example.allmyreview.MovieRetrofit.RetrofitClient
+import kotlinx.coroutines.*
+
+class DetailViewModel :ViewModel() {
+
+    val TAG = "DetailViewModel"
+    var job: Job? = null
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        Log.e(TAG, "Exception: ${throwable.localizedMessage}")
+    }
+    private val DetailService = RetrofitClient.getRetrofitService_Detail()
+    var data = MutableLiveData<DetailMovie>()
+
+    fun refresh(id : Int) {
+        getMovieDetail(id)
+    }
+    private fun getMovieDetail(id: Int) {
+        job = CoroutineScope(Dispatchers.IO + exceptionHandler).launch {
+            val response = DetailService.getMovieDetail(id)
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    data.postValue(response.body())
+                } else {
+                    Log.e(TAG, "Error: ${response.message()}")
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/allmyreview/movieDetail/MovieDetailActivity.kt
@@ -0,0 +1,79 @@
+package com.example.allmyreview.movieDetail
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.example.allmyreview.Genres
+import com.example.allmyreview.R
+import com.example.allmyreview.addReview.AddReviewActivity
+import com.example.allmyreview.databinding.ActivityMovieDetailBinding
+import kotlinx.coroutines.*
+
+class MovieDetailActivity : AppCompatActivity() {
+
+    private val TAG = "MovieDetailActivity"
+    private lateinit var binding: ActivityMovieDetailBinding
+    private lateinit var detailViewModel: DetailViewModel
+    private var id =0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMovieDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        id = intent.getIntExtra("movieId", 0)
+        detailViewModel = ViewModelProvider(this)[DetailViewModel::class.java]
+        detailViewModel.refresh(id)
+        observeViewModel()
+    }
+
+    @SuppressLint("SetTextI18n")
+    fun observeViewModel(){
+
+        detailViewModel.data.observe(this, Observer {
+            it?.let {
+                val detailData =detailViewModel.data
+                Log.d(TAG,"movie detail Data = "+detailData.value)
+                binding.movieTitle.text = "\"" + detailData.value!!.title + "\""
+                binding.movieOriginalTitle.text = detailData.value!!.original_title
+                val url = "https://image.tmdb.org/t/p/original" + detailData.value!!.backdrop_path
+                Glide.with(this).load(url)
+                    .placeholder(R.drawable.ic_launcher_foreground)
+                    .centerCrop()
+                    .into(binding.moviePoster)
+                binding.movieTagLine.text =
+                    if (detailData.value?.tagline.equals("")) "공개된 tagline이 없어요 :-(" else detailData.value?.tagline
+                binding.movieOverview.text =
+                    if (detailData.value?.overview.equals("")) "공개된 소개글이 없어요 :-(" else detailData.value?.overview
+
+                binding.movieReleaseDate.text=detailData.value?.release_date
+                binding.movieGenres.text=getGenres(detailData.value!!.genres)
+                binding.movieRunnigTime.text= detailData.value!!.runtime.toString()+"분"
+
+
+                binding.scrollview.visibility= View.VISIBLE
+            }
+
+        })
+    }
+
+    fun getGenres(genres: List<Genres>): String{
+        var str =""
+        for(i in 0 until genres.size)
+           str+=(genres.get(i).name+" ")
+        return str
+    }
+
+    fun addReviewBtn(view: View) {
+        val intent = Intent(this, AddReviewActivity::class.java)
+        intent.putExtra("movieId",id)
+        startActivity(intent)
+    }
+
+}
