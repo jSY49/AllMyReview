44095e9530e2ffd552d16e961bc398a74e8ce0ea
jSY49 <96411866+jSY49@users.noreply.github.com>
2023-03-03 12:10:20 +0900

    리뷰 삭제 추가

---
+++ app/src/main/java/com/example/allmyreview/reviewDetail/DeleteReviewViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.allmyreview.reviewDetail
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.allmyreview.reviewRetrofit.ReviewRetrofitClient
+import kotlinx.coroutines.*
+
+class DeleteReviewViewModel : ViewModel() {
+    val TAG = "deleteReviewViewModel"
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        Log.e(TAG, "Exception: ${throwable.localizedMessage}")
+    }
+    var state= MutableLiveData<Boolean>()
+    private val ReviewService = ReviewRetrofitClient.getRetrofitService()
+
+    fun refresh(reviewId: String) {
+        deleteReview(reviewId)
+    }
+
+    private fun deleteReview(reviewId: String) {
+        CoroutineScope(Dispatchers.IO+exceptionHandler).launch {
+            val call = ReviewService.deleteReview(reviewId)
+            withContext(Dispatchers.Main){
+                if (call.isSuccessful) {
+                    state.postValue(call.body()!!.success)
+                    Log.e(TAG, "delete reveiw call is Successed : ${call.body()}")
+                } else {
+                    state.postValue(false)
+                    Log.e(TAG, "delete reveiw Failed : ${call.raw()}")
+                }
+            }
+        }
+    }
+}
